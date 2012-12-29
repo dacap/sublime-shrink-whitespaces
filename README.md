1b356b7c6936d493455171b9d23ff8a9ad63762a
David Capello <davidcapello@gmail.com>
2012-12-28 21:36:16 -0300

    First version

---
+++ README.md
@@ -0,0 +1,12 @@
+# Shrink Whitespaces
+
+This plugin gives you `shrink_whitespaces` command for (Sublime Text 2)[http://www.sublimetext.com/]. With it you can:
+* Convert a set of blank lines in just one blank line, or
+* Remove the blank line that is between two paragraphs, or
+* Remove a set of whitespaces between two words, or
+* Remove the whitespace between two letters.
+The action depends on the context where the command is used.
+
+Default keyboard shortcut: <kbd>Alt+W</kbd>
+
+Based on [ErgoEmacs](http://www.ergoemacs.org/)'s [shrink-whitespaces](http://code.google.com/p/ergoemacs/issues/detail?id=13) function, which is a combination of three Emacs functions: just-one-space, delete-blank-lines, delete-horizontal-space.
---
+++ ShrinkWhitespaces.py
@@ -0,0 +1,82 @@
+# Sublime Shrink Whitespaces
+# Copyright (C) 2011-2012 David Capello
+#
+# http://github.com/dacap/sublime-shrink-whitespaces
+
+import sublime, sublime_plugin
+
+def expand_region_in_white_chars(view, reg):
+    a = reg.a
+    b = reg.b
+
+    # Expand to the beginning of the line
+    while True:
+        c = view.substr(sublime.Region(a-1, a))
+        if c == " " or c == "\t":
+            a -= 1
+        else:
+            break
+
+    # Expand to the end of the line
+    while True:
+        c = view.substr(sublime.Region(b+1, b))
+        if c == " " or c == "\t":
+            b += 1
+        else:
+            break
+
+    return sublime.Region(a, b)
+
+def expand_region_in_white_lines(view, reg):
+    # Cover previous white lines
+    while True:
+        newreg = expand_region_in_white_chars(view, sublime.Region(reg.a-1, reg.a-1))
+        fulllinereg = view.full_line(newreg.a)
+        if newreg.a == fulllinereg.a and newreg.b == fulllinereg.b-1:
+            reg = reg.cover(fulllinereg)
+        else:
+            break
+
+    # Cover next white lines
+    while True:
+        newreg = expand_region_in_white_chars(view, sublime.Region(reg.b, reg.b))
+        fulllinereg = view.full_line(newreg.a)
+        if newreg.a == fulllinereg.a and newreg.b == fulllinereg.b-1:
+            reg = reg.cover(fulllinereg)
+        else:
+            break
+
+    return reg
+
+class ShrinkWhitespacesCommand(sublime_plugin.TextCommand):
+    """Removes whitespaces and blank lines."""
+
+    def run(self, edit):
+        eraselines = False
+
+        sel = self.view.sel()
+        regions = []
+        for r in self.view.sel():
+            newreg = expand_region_in_white_chars(self.view, r)
+            fulllinereg = self.view.full_line(newreg.a)
+            if newreg.a == fulllinereg.a and newreg.b == fulllinereg.b-1:
+                eraselines = True
+                newreg = expand_region_in_white_lines(self.view, fulllinereg)
+
+            regions.append(newreg)
+
+        self.view.sel().clear()
+        for r in regions:
+            self.view.sel().add(r)
+
+        for r in self.view.sel():
+            newlines = self.view.substr(r).count('\n')
+            if newlines > 1:
+                self.view.replace(edit, r, '\n')
+            elif not eraselines and r.b - r.a > 1:
+                self.view.replace(edit, r, ' ')
+            else:
+                self.view.erase(edit, r)
+
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(regions[0].a, regions[0].a))
